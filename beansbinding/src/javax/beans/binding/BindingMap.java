233fa70cbfc0e75e20ba6c692e316640c2704ddd
shan_man <shan_man@729a9e19-9bed-43a5-b5a3-96140a88f07f>
2007-06-13 17:38:30 +0000

    Checked in BindingMap - not used yet.

---
+++ beansbinding/src/javax/beans/binding/BindingMap.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2006-2007 Sun Microsystems, Inc. All rights reserved. Use is
+ * subject to license terms.
+ */
+
+package javax.beans.binding;
+
+import java.util.*;
+
+/**
+ *
+ * @author Shannon Hickey
+ */
+public class BindingMap extends LinkedHashMap<String, Binding> {
+    
+    /** Creates a new instance of BindingMap */
+    public BindingMap() {
+    }
+    
+}
