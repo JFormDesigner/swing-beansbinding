2bdd01f57a21c5da15ab24031c43bd9689ced166
shan_man <shan_man@729a9e19-9bed-43a5-b5a3-96140a88f07f>
2007-07-10 20:30:44 +0000

    Working on the new BeanProperty implementation.

--- beansbinding/src/javax/beans/binding/BeanProperty.java
+++ beansbinding/src/javax/beans/binding/BeanProperty.java
@@ -7,34 +7,31 @@ package javax.beans.binding;
 
 import java.beans.PropertyChangeSupport;
 import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
 import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Map;
-
+import java.util.HashMap;
+import java.util.Collections;
 import com.sun.java.util.ObservableMap;
 import com.sun.java.util.ObservableMapListener;
-import javax.beans.binding.ext.PropertyDelegateFactory;
 
 public final class BeanProperty<S, V> implements Property<S, V> {
 
-    private S source;
     private final PropertyPath path;
+    private final Object[] sources;
     private PropertyChangeSupport support;
     private boolean isListening = false;
-
-
-    //private final Object[] sources;
-    //private final boolean emptySourcePath;
-    //private boolean bound;
+    private final boolean emptySourcePath;
     //private ChangeHandler changeHandler;
-    //private boolean ignoreChange;
-    //private Delegate delegate;
+    private boolean ignoreChange;
+    private Validator<V> validator;
+    private HashMap<Class<?>, Converter<?, V>> converters;
 
     public BeanProperty(String path, S source) {
         this.path = PropertyPath.createPropertyPath(path);
-        this.source = source;
 
         if (path.length() > 0) {
             sources = new Object[path.length()];
@@ -44,27 +41,6 @@ public final class BeanProperty<S, V> implements Property<S, V> {
             sources = new Object[] { source };
             emptySourcePath = true;
         }
-    }
-
-    public void setSource(S source) {
-        boolean wasListening = isListening;
-        V oldValue = null;
-
-        if (wasListening) {
-            stopListening();
-            oldValue = getValue();
-        }
-
-        this.source = source;
-
-        if (wasListening) {
-            startListening();
-            firePropertyChange(oldValue, getValue());
-        }
-    }
-
-    public S getSource() {
-        return source;
     }
 
     public Class<? extends V> getValueType() {
@@ -90,7 +66,7 @@ public final class BeanProperty<S, V> implements Property<S, V> {
         return false;
     }
 
-    public boolean isCompletePath() {
+    public boolean isComplete() {
         return false;
     }
 
@@ -147,11 +123,108 @@ public final class BeanProperty<S, V> implements Property<S, V> {
     }
 
     public String toString() {
-        StringBuilder ret = new StringBuilder(sourceClass.getName());
-        if (path != null) {
-            ret.append('.').append(path);
-        }
-        return ret.toString();
+        String className = (sources[0] == null ? "" : sources[0].getClass().getName());
+        return path == null ? className
+                            : className + '.' + path;
     }
 
+    public void setSource(S source) {
+        boolean wasListening = isListening;
+        V oldValue = null;
+
+        if (wasListening) {
+            stopListening();
+            oldValue = getValue();
+        }
+
+        sources[0] = source;
+
+        if (wasListening) {
+            startListening();
+            firePropertyChange(oldValue, getValue());
+        }
+    };
+
+    public S getSource() {
+        return (S)sources[0];
+    }
+    
+    public void setValidator(Validator<V> validator) {
+        this.validator = validator;
+    }
+
+    public Validator<V> getValidator() {
+        return validator;
+    }
+
+    public <F> void putConverter(Class<F> otherType, Converter<F, V> converter) {
+        if (otherType == null) {
+            throw new IllegalArgumentException("Must supply type");
+        }
+
+        if (converter == null) {
+            if (converters != null) {
+                converters.remove(otherType);
+            }
+        } else {
+            if (converters == null) {
+                converters = new HashMap<Class<?>, Converter<?, V>>();
+            }
+            converters.put(otherType, converter);
+        }
+    }
+
+    public <F> Converter<F, V> getConverter(Class<F> otherType) {
+        if (converters == null) {
+            return null;
+        }
+
+        return (Converter<F, V>)converters.get(otherType);
+    }
+
+    public Map<Class<?>, Converter<?, V>> getConverters() {
+        return Collections.unmodifiableMap(converters);
+    }
+
+/*
+    private final class ChangeHandler implements PropertyChangeListener,
+            ObservableMapListener {
+        public void propertyChange(PropertyChangeEvent e) {
+            if (!ignoreChange) {
+                Object source = e.getSource();
+                int index = getSourceIndex(e.getSource());
+                if (index != -1) {
+                    String propertyName = e.getPropertyName();
+                    if (propertyName == null ||
+                            path.get(index).equals(propertyName)) {
+                        Object newValue = e.getNewValue();
+                        if (newValue == null) {
+                            // A PropertyChangeEvent with a null value can mean
+                            // two things: either the new value is null, or it's
+                            // too expensive to calculate the new value. This
+                            // assumes it's the later.
+                            sourceValueChanged(index, source);
+                        } else {
+                            sourceValueChanged(index + 1, newValue);
+                        }
+                    } // else case means a different property changes
+                } else {
+                }
+            }
+        }
+
+        public void mapKeyValueChanged(ObservableMap map, Object key,
+                Object lastValue) {
+            mapValueChanged(map, key);
+        }
+
+        public void mapKeyAdded(ObservableMap map, Object key) {
+            mapValueChanged(map, key);
+        }
+
+        public void mapKeyRemoved(ObservableMap map, Object key, Object value) {
+            mapValueChanged(map, key);
+        }
+    }
+*/
 }
