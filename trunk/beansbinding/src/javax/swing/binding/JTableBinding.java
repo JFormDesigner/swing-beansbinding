1a87def176e6bf0bb5303f27668e6940c2acc74a
shan_man <shan_man@729a9e19-9bed-43a5-b5a3-96140a88f07f>
2007-08-02 14:58:58 +0000

    More work on JTableBinding.

--- trunk/beansbinding/src/javax/swing/binding/JTableBinding.java
+++ trunk/beansbinding/src/javax/swing/binding/JTableBinding.java
@@ -7,17 +7,20 @@ package javax.swing.binding;
 
 import javax.beans.binding.*;
 import javax.swing.*;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author Shannon Hickey
  */
-public final class JTableBinding<T> extends Binding<List<T>, List> {
+public final class JTableBinding<T> extends SwingBinding<List<T>, List> {
 
     private ElementsProperty<JTable> ep;
     private Handler handler = new Handler();
     private JTable table;
     private List<T> elements;
+    private boolean editable;
+    private boolean editableSet;
+    private List columnBindings = new ArrayList<TableColumnBinding>();
 
     public JTableBinding(List<T> source, JTable target) {
         this(null, source, target);
@@ -55,14 +58,78 @@ public final class JTableBinding<T> extends Binding<List<T>, List> {
         setup();
     }
 
+    public void setEditable(boolean editable) {
+        throwIfBound();
+        this.editable = editable;
+        this.editableSet = true;
+    }
+
+    public boolean isEditable() {
+        return editable;
+    }
+
+    public boolean isEditableSet() {
+        return editableSet;
+    }
+
+    public TableColumnBinding addColumnBinding(SourceableProperty source, int column) {
+        throwIfBound();
+        TableColumnBinding binding = new TableColumnBinding(source, column);
+        columnBindings.add(binding);
+        return binding;
+    }
+
+    public void removeColumnBinding(int column) {
+        throwIfBound();
+    }
+
+    public TableColumnBinding[] getColumnBindings() {
+        return null;
+    }
+
     private void setup() {
         super.setAutoUpdateStrategy(AutoUpdateStrategy.READ);
         prepareElementsProperty();
     }
-    
+
     private void prepareElementsProperty() {
         ep = (ElementsProperty)getTarget();
         ep.addPropertyStateListener(handler);
+    }
+
+    private final Property DUMMY_PROPERTY = new BeanProperty("DUMMY");
+
+    public final class TableColumnBinding extends SwingBinding {
+        private Class<?> columnClass;
+        private boolean editable;
+        private int column;
+
+        public TableColumnBinding(SourceableProperty prop, int column) {
+            super(prop, DUMMY_PROPERTY);
+            this.column = column;
+        }
+
+        public int getColumn() {
+            return column;
+        }
+        
+        public void setColumnClass(Class<?> columnClass) {
+        }
+
+        public Class<?> getColumnClass() {
+            return null;
+        }
+        
+        public void setEditable(boolean editable) {
+        }
+
+        public boolean isEditable() {
+            return false;
+        }
+
+        public boolean isEditableSet() {
+            return false;
+        }
     }
 
     private class Handler implements PropertyStateListener {
