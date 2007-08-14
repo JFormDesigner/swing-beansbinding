3c53c9e71fd0cb134b7dcf853a3b169be0c7dec0
shan_man <shan_man@729a9e19-9bed-43a5-b5a3-96140a88f07f>
2007-08-14 20:57:11 +0000

    Continuing cleanup effort.

--- trunk/beansbinding/src/com/sun/el/lang/EvaluationContext.java
+++ trunk/beansbinding/src/com/sun/el/lang/EvaluationContext.java
@@ -7,7 +7,6 @@ package com.sun.el.lang;
 
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import javax.el.ELContext;
@@ -24,40 +23,25 @@ public final class EvaluationContext extends ELContext {
 
     private final VariableMapper varMapper;
     
-    private final Expression expression;
-    
     private final Set<Expression.ResolvedObject> resolvedObjects;
     
-    public int listIndex;
-
-    private boolean incompletePath;
-
     public EvaluationContext(ELContext elContext, FunctionMapper fnMapper,
-            VariableMapper varMapper, Expression expression) {
-        this(elContext, fnMapper, varMapper, expression, false);
+            VariableMapper varMapper) {
+        this(elContext, fnMapper, varMapper, false);
     }
 
     public EvaluationContext(ELContext elContext, FunctionMapper fnMapper,
-            VariableMapper varMapper, Expression expression, 
-            boolean trackResolvedObjects) {
+            VariableMapper varMapper, boolean trackResolvedObjects) {
         this.elContext = elContext;
         this.fnMapper = fnMapper;
         this.varMapper = varMapper;
-        this.expression = expression;
-        Set<Expression.ResolvedObject> resolvedObjects;
         if (trackResolvedObjects) {
             resolvedObjects = new LinkedHashSet<Expression.ResolvedObject>(1);
         } else {
             resolvedObjects = null;
         }
-        this.resolvedObjects = resolvedObjects;
-        listIndex = 0;
     }
     
-    public Expression getExpression() {
-        return expression;
-    }
-
     public ELContext getELContext() {
         return this.elContext;
     }
@@ -90,20 +74,6 @@ public final class EvaluationContext extends ELContext {
         this.elContext.setPropertyResolved(resolved);
     }
     
-    public void setIncompletePath(boolean incompletePath) {
-        this.incompletePath = incompletePath;
-    }
-    
-    public boolean getIncompletePath() {
-        return incompletePath;
-    }
-
-    public void resolvingList(List list) {
-        if (resolvedObjects != null) {
-            resolvedObjects.add(new Expression.ResolvedList(list));
-        }
-    }
-
     public void resolvingProperty(Object base, Object property) {
         if (resolvedObjects != null) {
             resolvedObjects.add(new Expression.ResolvedProperty(base, property));
@@ -117,19 +87,4 @@ public final class EvaluationContext extends ELContext {
         return null;
     }
     
-    public void setResolvingListIndex(int listIndex) {
-        this.listIndex = listIndex;
-    }
-
-    public int getResolvingListIndex() {
-        return listIndex;
-    }
-
-    public void reset() {
-        listIndex = 0;
-        incompletePath = false;
-    }
-    
-    
-
 }
