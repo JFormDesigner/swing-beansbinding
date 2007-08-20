7ec936fe8099f2e92df4a7b489957de1d30502ef
shan_man <shan_man@729a9e19-9bed-43a5-b5a3-96140a88f07f>
2007-08-20 02:34:21 +0000

    Working on slider delegate.

---
+++ trunk/beansbinding/src/com/sun/java/swing/binding/JSliderDelegateProvider.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2006-2007 Sun Microsystems, Inc. All rights reserved. Use is
+ * subject to license terms.
+ */
+
+package com.sun.java.swing.binding;
+
+import javax.beans.binding.ext.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.beans.*;
+
+/**
+ * @author Shannon Hickey
+ */
+public final class JSliderDelegateProvider implements BeanDelegateProvider {
+
+    private static final String PROPERTY_BASE = "value";
+    private static final String IGNORE_ADJUSTING = PROPERTY_BASE + "_IGNORE_ADJUSTING";
+    private static final String ON_ADJUSTING = PROPERTY_BASE + "_ON_ADJUSTING";
+
+    public static final class Delegate extends DelegateBase {
+        private JSlider slider;
+        private Handler handler;
+        private int cachedValue;
+
+        private Delegate(JSlider slider, String property) {
+            super(property);
+            this.slider = slider;
+        }
+
+        public int getValue() {
+            return slider.getValue();
+        }
+
+        public void setValue(int value) {
+            slider.setValue(value);
+        }
+
+        protected void listeningStarted() {
+            handler = new Handler();
+            cachedValue = getValue();
+            slider.addChangeListener(handler);
+            slider.addPropertyChangeListener("model", handler);
+        }
+
+        protected void listeningStopped() {
+            slider.removeChangeListener(handler);
+            slider.removePropertyChangeListener("model", handler);
+            handler = null;
+        }
+        
+        private class Handler implements ChangeListener, PropertyChangeListener {
+            private void sliderValueChanged() {
+                int oldValue = cachedValue;
+                cachedValue = getValue();
+                firePropertyChange(oldValue, cachedValue);
+            }
+
+            public void stateChanged(ChangeEvent ce) {
+                if ((property == PROPERTY_BASE || property == IGNORE_ADJUSTING) && slider.getValueIsAdjusting()) {
+                    return;
+                }
+
+                sliderValueChanged();
+            }
+
+            public void propertyChange(PropertyChangeEvent pe) {
+                sliderValueChanged();
+            }
+        }
+    }
+
+    public boolean providesDelegate(Class<?> type, String property) {
+        if (!JSlider.class.isAssignableFrom(type)) {
+            return false;
+        }
+
+        return property == PROPERTY_BASE ||
+               property == IGNORE_ADJUSTING ||
+               property == ON_ADJUSTING;
+    }
+
+    public Object createPropertyDelegate(Object source, String property) {
+        if (!providesDelegate(source.getClass(), property)) {
+            throw new IllegalArgumentException();
+        }
+
+        return new Delegate((JSlider)source, property);
+    }
+
+    public Class<?> getPropertyDelegateClass(Class<?> type) {
+        return AbstractButtonDelegateProvider.Delegate.class;
+    }
+
+}
