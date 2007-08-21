154e3dd63c6a2f5d0d7054b2364eea476cfa70e4
shan_man <shan_man@729a9e19-9bed-43a5-b5a3-96140a88f07f>
2007-08-21 20:22:59 +0000

    selectedElements property for JTable

--- trunk/beansbinding/src/com/sun/java/swing/binding/JTableAdapterProvider.java
+++ trunk/beansbinding/src/com/sun/java/swing/binding/JTableAdapterProvider.java
@@ -12,19 +12,24 @@ import sun.swing.binding.*;
 import javax.swing.event.*;
 import javax.swing.table.*;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  * @author Shannon Hickey
  */
 public final class JTableAdapterProvider implements BeanAdapterProvider {
 
-    private static final String PROPERTY_BASE = "selectedElement";
-    private static final String IGNORE_ADJUSTING = PROPERTY_BASE + "_IGNORE_ADJUSTING";
+    private static final String SELECTED_ELEMENT_P = "selectedElement";
+    private static final String SELECTED_ELEMENTS_P = "selectedElements";
+    private static final String SELECTED_ELEMENT_IA_P = SELECTED_ELEMENT_P + "_IGNORE_ADJUSTING";
+    private static final String SELECTED_ELEMENTS_IA_P = SELECTED_ELEMENTS_P + "_IGNORE_ADJUSTING";
 
     public final class Adapter extends BeanAdapterBase {
         private JTable table;
         private Handler handler;
         private Object cachedElement;
+        private List<Object> cachedElements;
 
         private Adapter(JTable table, String property) {
             super(property);
@@ -39,9 +44,18 @@ public final class JTableAdapterProvider implements BeanAdapterProvider {
             return getSelectedElement();
         }
 
+        public List<Object> getSelectedElements() {
+            return JTableAdapterProvider.getSelectedElements(table);
+        }
+
+        public List<Object> getSelectedElements_IGNORE_ADJUSTING() {
+            return getSelectedElements();
+        }
+
         protected void listeningStarted() {
             handler = new Handler();
             cachedElement = JTableAdapterProvider.getSelectedElement(table);
+            cachedElements = JTableAdapterProvider.getSelectedElements(table);
             table.addPropertyChangeListener("selectionModel", handler);
             table.getSelectionModel().addListSelectionListener(handler);
         }
@@ -50,18 +64,24 @@ public final class JTableAdapterProvider implements BeanAdapterProvider {
             table.getSelectionModel().removeListSelectionListener(handler);
             table.removePropertyChangeListener("selectionModel", handler);
             cachedElement = null;
+            cachedElements = null;
             handler = null;
         }
 
         private class Handler implements ListSelectionListener, PropertyChangeListener {
             private void tableSelectionChanged() {
                 Object oldElement = cachedElement;
+                Object oldElements = cachedElements;
                 cachedElement = getSelectedElement();
+                cachedElements = getSelectedElements();
                 firePropertyChange(oldElement, cachedElement);
+                firePropertyChange(oldElements, cachedElements);
             }
 
             public void valueChanged(ListSelectionEvent e) {
-                if (property == IGNORE_ADJUSTING && e.getValueIsAdjusting()) {
+                if ((property == SELECTED_ELEMENT_IA_P || property == SELECTED_ELEMENTS_IA_P)
+                        && e.getValueIsAdjusting()) {
+
                     return;
                 }
 
@@ -72,6 +92,30 @@ public final class JTableAdapterProvider implements BeanAdapterProvider {
                 tableSelectionChanged();
             }
         }
+    }
+
+    private static List<Object> getSelectedElements(JTable table) {
+        assert table != null;
+
+        ListSelectionModel selectionModel = table.getSelectionModel();
+        int min = selectionModel.getMinSelectionIndex();
+        int max = selectionModel.getMaxSelectionIndex();
+
+        List<Object> newSelection;
+
+        if (min < 0 || max < 0) {
+            return new ArrayList<Object>(0);
+        }
+        
+        ArrayList<Object> elements = new ArrayList<Object>(max - min + 1);
+
+        for (int i = min; i <= max; i++) {
+            if (selectionModel.isSelectedIndex(i)) {
+                elements.add(getElement(table, i));
+            }
+        }
+        
+        return elements;
     }
 
     private static Object getSelectedElement(JTable table) {
@@ -85,6 +129,10 @@ public final class JTableAdapterProvider implements BeanAdapterProvider {
             return null;
         }
 
+        return getElement(table, index);
+    }
+
+    private static Object getElement(JTable table, int index) {
         TableModel model = table.getModel();
         if (model instanceof ListBindingManager) {
             return ((ListBindingManager)model).getElement(index);
@@ -93,15 +141,12 @@ public final class JTableAdapterProvider implements BeanAdapterProvider {
             // PENDING(shannonh) - need to support editing values in this map!
             HashMap map = new HashMap(columnCount);
             for (int i = 0; i < columnCount; i++) {
-                // PENDING(shannonh) - find better identifiers
-                // can't use plain column name since it might be a bad identifier
-                // but can we convert it to a propert identifier?
                 map.put("column" + i, model.getValueAt(index, i));
             }
             return map;
         }
     }
-    
+
     public boolean providesAdapter(Class<?> type, String property) {
         property = property.intern();
 
@@ -109,11 +154,13 @@ public final class JTableAdapterProvider implements BeanAdapterProvider {
             return false;
         }
 
-        return property == PROPERTY_BASE ||
-               property == IGNORE_ADJUSTING;
+        return property == SELECTED_ELEMENT_P ||
+               property == SELECTED_ELEMENT_IA_P ||
+               property == SELECTED_ELEMENTS_P ||
+               property == SELECTED_ELEMENTS_IA_P;
                  
     }
-    
+
     public Object createAdapter(Object source, String property) {
         if (!providesAdapter(source.getClass(), property)) {
             throw new IllegalArgumentException();
