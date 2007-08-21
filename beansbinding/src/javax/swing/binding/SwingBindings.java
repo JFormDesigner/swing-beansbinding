bc0f6bb0d5a0823d686f98fbf2d290fb4949263e
shan_man <shan_man@729a9e19-9bed-43a5-b5a3-96140a88f07f>
2007-08-21 22:33:31 +0000

    ObjectProperty gets create method.

--- beansbinding/src/javax/swing/binding/SwingBindings.java
+++ beansbinding/src/javax/swing/binding/SwingBindings.java
@@ -17,29 +17,29 @@ public class SwingBindings {
     private SwingBindings() {}
 
     public static <E> JListBinding<E, List<E>, JList> createListBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, JList targetJList) {
-        return new JListBinding<E, List<E>, JList>(strategy, sourceList, new ObjectProperty<List<E>>(), targetJList, new ObjectProperty<JList>(), null);
+        return new JListBinding<E, List<E>, JList>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetJList, ObjectProperty.<JList>create(), null);
     }
 
     public static <E> JListBinding<E, List<E>, JList> createListBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, JList targetJList, String name) {
-        return new JListBinding<E, List<E>, JList>(strategy, sourceList, new ObjectProperty<List<E>>(), targetJList, new ObjectProperty<JList>(), name);
+        return new JListBinding<E, List<E>, JList>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetJList, ObjectProperty.<JList>create(), name);
     }
 
 
     public static <E, SS> JListBinding<E, SS, JList> createListBinding(AutoBinding.UpdateStrategy strategy, SS sourceObject, Property<SS, List<E>> sourceListProperty, JList targetJList) {
-        return new JListBinding<E, SS, JList>(strategy, sourceObject, sourceListProperty, targetJList, new ObjectProperty<JList>(), null);
+        return new JListBinding<E, SS, JList>(strategy, sourceObject, sourceListProperty, targetJList, ObjectProperty.<JList>create(), null);
     }
     
     public static <E, SS> JListBinding<E, SS, JList> createListBinding(AutoBinding.UpdateStrategy strategy, SS sourceObject, Property<SS, List<E>> sourceListProperty, JList targetJList, String name) {
-        return new JListBinding<E, SS, JList>(strategy, sourceObject, sourceListProperty, targetJList, new ObjectProperty<JList>(), name);
+        return new JListBinding<E, SS, JList>(strategy, sourceObject, sourceListProperty, targetJList, ObjectProperty.<JList>create(), name);
     }
 
 
     public static <E, TS> JListBinding<E, List<E>, TS> createListBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, TS targetObject, Property<TS, ? extends JList> targetJListProperty) {
-        return new JListBinding<E, List<E>, TS>(strategy, sourceList, new ObjectProperty<List<E>>(), targetObject, targetJListProperty, null);
+        return new JListBinding<E, List<E>, TS>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetObject, targetJListProperty, null);
     }
     
     public static <E, TS> JListBinding<E, List<E>, TS> createListBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, TS targetObject, Property<TS, ? extends JList> targetJListProperty, String name) {
-        return new JListBinding<E, List<E>, TS>(strategy, sourceList, new ObjectProperty<List<E>>(), targetObject, targetJListProperty, name);
+        return new JListBinding<E, List<E>, TS>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetObject, targetJListProperty, name);
     }
 
 
@@ -54,29 +54,29 @@ public class SwingBindings {
     
 
     public static <E> JTableBinding<E, List<E>, JTable> createTableBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, JTable targetJTable) {
-        return new JTableBinding<E, List<E>, JTable>(strategy, sourceList, new ObjectProperty<List<E>>(), targetJTable, new ObjectProperty<JTable>(), null);
+        return new JTableBinding<E, List<E>, JTable>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetJTable, ObjectProperty.<JTable>create(), null);
     }
 
     public static <E> JTableBinding<E, List<E>, JTable> createTableBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, JTable targetJTable, String name) {
-        return new JTableBinding<E, List<E>, JTable>(strategy, sourceList, new ObjectProperty<List<E>>(), targetJTable, new ObjectProperty<JTable>(), name);
+        return new JTableBinding<E, List<E>, JTable>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetJTable, ObjectProperty.<JTable>create(), name);
     }
 
 
     public static <E, SS> JTableBinding<E, SS, JTable> createTableBinding(AutoBinding.UpdateStrategy strategy, SS sourceObject, Property<SS, List<E>> sourceListProperty, JTable targetJTable) {
-        return new JTableBinding<E, SS, JTable>(strategy, sourceObject, sourceListProperty, targetJTable, new ObjectProperty<JTable>(), null);
+        return new JTableBinding<E, SS, JTable>(strategy, sourceObject, sourceListProperty, targetJTable, ObjectProperty.<JTable>create(), null);
     }
 
     public static <E, SS> JTableBinding<E, SS, JTable> createTableBinding(AutoBinding.UpdateStrategy strategy, SS sourceObject, Property<SS, List<E>> sourceListProperty, JTable targetJTable, String name) {
-        return new JTableBinding<E, SS, JTable>(strategy, sourceObject, sourceListProperty, targetJTable, new ObjectProperty<JTable>(), name);
+        return new JTableBinding<E, SS, JTable>(strategy, sourceObject, sourceListProperty, targetJTable, ObjectProperty.<JTable>create(), name);
     }
 
     
     public static <E, TS> JTableBinding<E, List<E>, TS> createTableBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, TS targetObject, Property<TS, ? extends JTable> targetJTableProperty) {
-        return new JTableBinding<E, List<E>, TS>(strategy, sourceList, new ObjectProperty<List<E>>(), targetObject, targetJTableProperty, null);
+        return new JTableBinding<E, List<E>, TS>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetObject, targetJTableProperty, null);
     }
 
     public static <E, TS> JTableBinding<E, List<E>, TS> createTableBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, TS targetObject, Property<TS, ? extends JTable> targetJTableProperty, String name) {
-        return new JTableBinding<E, List<E>, TS>(strategy, sourceList, new ObjectProperty<List<E>>(), targetObject, targetJTableProperty, name);
+        return new JTableBinding<E, List<E>, TS>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetObject, targetJTableProperty, name);
     }
 
 
