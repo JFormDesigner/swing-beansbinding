48002756fa69500bf6b8310a67602d0db316b99c
shan_man <shan_man@729a9e19-9bed-43a5-b5a3-96140a88f07f>
2007-08-22 17:54:19 +0000

    Working on JTable.

---
+++ beansbinding/src/org/jdesktop/beansbinding/AbstractBindingListener.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2007 Sun Microsystems, Inc. All rights reserved. Use is
+ * subject to license terms.
+ */
+
+package org.jdesktop.beansbinding;
+
+import java.util.EventListener;
+
+/**
+ * @author Shannon Hickey
+ */
+public abstract class AbstractBindingListener implements BindingListener {
+    public void bindingBecameBound(Binding binding) {}
+    public void bindingBecameUnbound(Binding binding) {}
+    public void syncFailed(Binding binding, Binding.SyncFailure... failures) {}
+    public void synced(Binding binding) {}
+    public void sourceEdited(Binding binding) {}
+    public void targetEdited(Binding binding) {}
+}
