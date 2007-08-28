8b228a9e3556545cf54830a0c198efd34a7bc560
shan_man <shan_man@729a9e19-9bed-43a5-b5a3-96140a88f07f>
2007-08-28 19:20:22 +0000

    PropertyHelper JavaDoc

--- trunk/beansbinding/src/org/jdesktop/beansbinding/PropertyHelper.java
+++ trunk/beansbinding/src/org/jdesktop/beansbinding/PropertyHelper.java
@@ -8,18 +8,45 @@ package org.jdesktop.beansbinding;
 import java.util.*;
 
 /**
+ * An abstract subclass of {@code Property} that helps with the management of
+ * {@code PropertyStateListeners} by implementing the methods for adding, removing,
+ * and getting listeners. {@code PropertyHelper} can be constructed
+ * to manage listeners for multiple source objects, or to ignore the source
+ * object argument when dealing with listeners and associate them directly with
+ * the {@code PropertyHelper} instance itself. This makes {@code PropertyHelper}
+ * useful as a base for both property types described in the documentation for
+ * {@code Property}.
+ * <p>
+ * {@code PropertyHelper} also provides, by way of the protected methods
+ * {@link #listeningStarted} and {@link #listeningStopped} a hook for subclasses
+ * to know when it's time to start tracking changes to a particular source object.
+ *
+ * @param <S> the type of source object that this {@code Property} operates on
+ * @param <V> the type of value that this {@code Property} represents
+ *
  * @author Shannon Hickey
  */
-// PENDING(shannonh) - needs a better name
 public abstract class PropertyHelper<S, V> extends Property<S, V> {
 
     private final boolean ignoresSource;
     private Object listeners;
 
+    /**
+     * Create a {@code PropertyHelper} that manages listeners for multiple
+     * source objects.
+     */
     public PropertyHelper() {
         this(false);
     }
 
+    /**
+     * Create a {@code PropertyHelper}, specifying whether it manages
+     * listeners for multiple source objects, or ignores the source object
+     * argument when dealing with listeners
+     *
+     * @param ignoresSource whether or not the source argument is ignored
+     *        when dealing with listeners
+     */
     public PropertyHelper(boolean ignoresSource) {
         this.ignoresSource = ignoresSource;
     }
@@ -56,22 +83,63 @@ public abstract class PropertyHelper<S, V> extends Property<S, V> {
         return list;
     }
 
+    /**
+     * {@inheritDoc}
+     * @throws UnsupportedOperationException {@inheritDoc}
+     */
     public abstract Class<? extends V> getWriteType(S source);
 
+    /**
+     * {@inheritDoc}
+     * @throws UnsupportedOperationException {@inheritDoc}
+     */
     public abstract V getValue(S source);
 
+    /**
+     * {@inheritDoc}
+     * @throws UnsupportedOperationException {@inheritDoc}
+     */
     public abstract void setValue(S source, V value);
 
+    /**
+     * {@inheritDoc}
+     * @throws UnsupportedOperationException {@inheritDoc}
+     */
     public abstract boolean isReadable(S source);
 
+    /**
+     * {@inheritDoc}
+     * @throws UnsupportedOperationException {@inheritDoc}
+     */
     public abstract boolean isWriteable(S source);
 
+    /**
+     * Called when this {@code PropertyHelper} changes from having
+     * no listeners installed for the given source object to
+     * having listeners installed for the given source object. This
+     * is the ideal time for subclasses to install any listeners needed
+     * to track change on the source object.
+     *
+     * @see #listeningStopped
+     */
     protected void listeningStarted(S source) {
     }
 
+    /**
+     * Called when this {@code PropertyHelper} changes from having
+     * listeners installed for the given source object to
+     * having no listeners installed for the given source object. This
+     * is the ideal time for subclasses to remove any listeners that
+     * they've installed to track changes on the source object.
+     *
+     * @see #listeningStopped
+     */
     protected void listeningStopped(S source) {
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public final void addPropertyStateListener(S source, PropertyStateListener listener) {
         if (listener == null) {
             return;
@@ -86,6 +154,9 @@ public abstract class PropertyHelper<S, V> extends Property<S, V> {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public final void removePropertyStateListener(S source, PropertyStateListener listener) {
         if (listener == null) {
             return;
@@ -106,6 +177,9 @@ public abstract class PropertyHelper<S, V> extends Property<S, V> {
         }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public final PropertyStateListener[] getPropertyStateListeners(S source) {
          List<PropertyStateListener> listeners = getListeners(source, false);
 
@@ -118,6 +192,15 @@ public abstract class PropertyHelper<S, V> extends Property<S, V> {
         return ret;
     }
 
+    /**
+     * Notify listeners that the state of this property has changed, as
+     * characterized by the given {@code PropertyStateEvent}. If this
+     * {@code PropertyHelper} is managing listeners for multiple sources, only
+     * the listeners associated with the object returned by the
+     * {@code PropertyStateEvent's getSourceObject()} method are notified.
+     *
+     * @param pse the {@code PropertyStateEvent} characterizing the state change
+     */
     protected final void firePropertyStateChange(PropertyStateEvent pse) {
         List<PropertyStateListener> listeners = getListeners((S)pse.getSourceObject(), false);
 
@@ -130,6 +213,14 @@ public abstract class PropertyHelper<S, V> extends Property<S, V> {
         }
     }
 
+    /**
+     * Returns whether or not there are any {@code PropertyStateListeners}
+     * installed for the given source object.
+     *
+     * @param the source object of interest
+     * @return whether or not there are any {@code PropertyStateListeners}
+     *         installed for the given source object
+     */
     public final boolean isListening(S source) {
          List<PropertyStateListener> listeners = getListeners(source, false);
          return listeners != null && listeners.size() != 0;
