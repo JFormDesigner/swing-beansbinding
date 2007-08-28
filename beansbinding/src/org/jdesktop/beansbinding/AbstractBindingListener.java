5fa402027a57ef03ea54954e7865d1242e05d4a0
shan_man <shan_man@729a9e19-9bed-43a5-b5a3-96140a88f07f>
2007-08-28 20:17:40 +0000

    JavaDoc for AbstractBindingListener

--- beansbinding/src/org/jdesktop/beansbinding/AbstractBindingListener.java
+++ beansbinding/src/org/jdesktop/beansbinding/AbstractBindingListener.java
@@ -8,13 +8,42 @@ package org.jdesktop.beansbinding;
 import java.util.EventListener;
 
 /**
+ * An abstract subclass of {@code BindingListener} that simplifies writing
+ * {@code BindingListeners} by allowing you to extend this class and re-implement
+ * only the methods you care about.
+ *
  * @author Shannon Hickey
  */
 public abstract class AbstractBindingListener implements BindingListener {
+
+    /**
+     * {@inheritDoc}
+     */
     public void bindingBecameBound(Binding binding) {}
+
+    /**
+     * {@inheritDoc}
+     */
     public void bindingBecameUnbound(Binding binding) {}
+
+    /**
+     * {@inheritDoc}
+     */
     public void syncFailed(Binding binding, Binding.SyncFailure... failures) {}
+
+    /**
+     * {@inheritDoc}
+     */
     public void synced(Binding binding) {}
+
+    /**
+     * {@inheritDoc}
+     */
     public void sourceEdited(Binding binding) {}
+
+    /**
+     * {@inheritDoc}
+     */
     public void targetEdited(Binding binding) {}
+
 }
