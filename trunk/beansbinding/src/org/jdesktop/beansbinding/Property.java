29468f6c6d697e315ae0e9100f29bb0360c582ff
shan_man <shan_man@729a9e19-9bed-43a5-b5a3-96140a88f07f>
2007-08-28 17:27:24 +0000

    JavaDoc beginning for Property.

--- trunk/beansbinding/src/org/jdesktop/beansbinding/Property.java
+++ trunk/beansbinding/src/org/jdesktop/beansbinding/Property.java
@@ -6,6 +6,16 @@
 package org.jdesktop.beansbinding;
 
 /**
+ * {@code Property} defines a uniform way to access the value of a property.
+ * A typical {@code Property} implemention is an immutable representation
+ * of a way to access some property derived from a source object. As such,
+ * all methods of this class take a source object as an argument.
+ * <p>
+ * A {@code Property} implementation may, however, be designed such that the
+ * {@code Property} itself stores the property value. In such a case, the
+ * {@code Property} implementation may ignore the source object. {@code Property}
+ * implementations should clearly document their behavior in this regard.
+ *
  * @author Shannon Hickey
  */
 public abstract class Property<S, V> {
