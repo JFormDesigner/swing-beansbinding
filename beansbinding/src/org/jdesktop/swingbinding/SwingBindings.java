e65440e0bc209830d2fc9b5188784b0272a8f47b
shan_man <shan_man@729a9e19-9bed-43a5-b5a3-96140a88f07f>
2007-09-04 13:26:14 +0000

    Finished SwingBindings JavaDoc.

--- beansbinding/src/org/jdesktop/swingbinding/SwingBindings.java
+++ beansbinding/src/org/jdesktop/swingbinding/SwingBindings.java
@@ -22,7 +22,7 @@ import org.jdesktop.beansbinding.Property;
 public class SwingBindings {
     
     private SwingBindings() {}
-
+    
     /**
      * Creates a {@code JListBinding} from direct references to a {@code List} and {@code JList}.
      *
@@ -34,7 +34,7 @@ public class SwingBindings {
     public static <E> JListBinding<E, List<E>, JList> createListBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, JList targetJList) {
         return new JListBinding<E, List<E>, JList>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetJList, ObjectProperty.<JList>create(), null);
     }
-
+    
     /**
      * Creates a named {@code JListBinding} from direct references to a {@code List} and {@code JList}.
      *
@@ -46,36 +46,92 @@ public class SwingBindings {
     public static <E> JListBinding<E, List<E>, JList> createListBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, JList targetJList, String name) {
         return new JListBinding<E, List<E>, JList>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetJList, ObjectProperty.<JList>create(), name);
     }
-
-
+    
+    
+    /**
+     * Creates a {@code JListBinding} from an object and property that resolves to a {@code List} and a direct reference to a {@code JList}.
+     *
+     * @param strategy the update strategy
+     * @param sourceObject the source object
+     * @param sourceListProperty a property on the source object that resolves to a {@code List}
+     * @param targetJList the target {@code JList}
+     * @return the {@code JListBinding}
+     */
     public static <E, SS> JListBinding<E, SS, JList> createListBinding(AutoBinding.UpdateStrategy strategy, SS sourceObject, Property<SS, List<E>> sourceListProperty, JList targetJList) {
         return new JListBinding<E, SS, JList>(strategy, sourceObject, sourceListProperty, targetJList, ObjectProperty.<JList>create(), null);
     }
     
+    /**
+     * Creates a named {@code JListBinding} from an object and property that resolves to a {@code List} and a direct reference to a {@code JList}.
+     *
+     * @param strategy the update strategy
+     * @param sourceObject the source object
+     * @param sourceListProperty a property on the source object that resolves to a {@code List}
+     * @param targetJList the target {@code JList}
+     * @return the {@code JListBinding}
+     */
     public static <E, SS> JListBinding<E, SS, JList> createListBinding(AutoBinding.UpdateStrategy strategy, SS sourceObject, Property<SS, List<E>> sourceListProperty, JList targetJList, String name) {
         return new JListBinding<E, SS, JList>(strategy, sourceObject, sourceListProperty, targetJList, ObjectProperty.<JList>create(), name);
     }
-
-
+    
+    
+    /**
+     * Creates a {@code JListBinding} from a direct reference to a {@code List} and an object and property that resolves to a {@code JList}.
+     *
+     * @param strategy the update strategy
+     * @param sourceList the source {@code List}
+     * @param targetObject the target object
+     * @param targetJListProperty a property on the target object that resolves to a {@code JList}
+     * @return the {@code JListBinding}
+     */
     public static <E, TS> JListBinding<E, List<E>, TS> createListBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, TS targetObject, Property<TS, ? extends JList> targetJListProperty) {
         return new JListBinding<E, List<E>, TS>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetObject, targetJListProperty, null);
     }
     
+    /**
+     * Creates a named {@code JListBinding} from a direct reference to a {@code List} and an object and property that resolves to a {@code JList}.
+     *
+     * @param strategy the update strategy
+     * @param sourceList the source {@code List}
+     * @param targetObject the target object
+     * @param targetJListProperty a property on the target object that resolves to a {@code JList}
+     * @return the {@code JListBinding}
+     */
     public static <E, TS> JListBinding<E, List<E>, TS> createListBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, TS targetObject, Property<TS, ? extends JList> targetJListProperty, String name) {
         return new JListBinding<E, List<E>, TS>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetObject, targetJListProperty, name);
     }
-
-
+    
+    
+    /**
+     * Creates a {@code JListBinding} from an object and property that resolves to a {@code List} and an object and property that resolves to a {@code JList}.
+     *
+     * @param strategy the update strategy
+     * @param sourceObject the source object
+     * @param sourceListProperty a property on the source object that resolves to a {@code List}
+     * @param targetObject the target object
+     * @param targetJListProperty a property on the target object that resolves to a {@code JList}
+     * @return the {@code JListBinding}
+     */
     public static <E, SS, TS> JListBinding<E, SS, TS> createListBinding(AutoBinding.UpdateStrategy strategy, SS sourceObject, Property<SS, List<E>> sourceListProperty, TS targetObject, Property<TS, ? extends JList> targetJListProperty) {
         return new JListBinding<E, SS, TS>(strategy, sourceObject, sourceListProperty, targetObject, targetJListProperty, null);
     }
-
+    
+    /**
+     * Creates a named {@code JListBinding} from an object and property that resolves to a {@code List} and an object and property that resolves to a {@code JList}.
+     *
+     * @param strategy the update strategy
+     * @param sourceObject the source object
+     * @param sourceListProperty a property on the source object that resolves to a {@code List}
+     * @param targetObject the target object
+     * @param targetJListProperty a property on the target object that resolves to a {@code JList}
+     * @return the {@code JListBinding}
+     */
     public static <E, SS, TS> JListBinding<E, SS, TS> createListBinding(AutoBinding.UpdateStrategy strategy, SS sourceObject, Property<SS, List<E>> sourceListProperty, TS targetObject, Property<TS, ? extends JList> targetJListProperty, String name) {
         return new JListBinding<E, SS, TS>(strategy, sourceObject, sourceListProperty, targetObject, targetJListProperty, name);
     }
-
     
-
+    
+    
     /**
      * Creates a {@code JTableBinding} from direct references to a {@code List} and {@code JTable}.
      *
@@ -87,7 +143,7 @@ public class SwingBindings {
     public static <E> JTableBinding<E, List<E>, JTable> createTableBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, JTable targetJTable) {
         return new JTableBinding<E, List<E>, JTable>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetJTable, ObjectProperty.<JTable>create(), null);
     }
-
+    
     /**
      * Creates a named {@code JTableBinding} from direct references to a {@code List} and {@code JTable}.
      *
@@ -99,8 +155,8 @@ public class SwingBindings {
     public static <E> JTableBinding<E, List<E>, JTable> createTableBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, JTable targetJTable, String name) {
         return new JTableBinding<E, List<E>, JTable>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetJTable, ObjectProperty.<JTable>create(), name);
     }
-
-
+    
+    
     /**
      * Creates a {@code JTableBinding} from an object and property that resolves to a {@code List} and a direct reference to a {@code JTable}.
      *
@@ -113,7 +169,7 @@ public class SwingBindings {
     public static <E, SS> JTableBinding<E, SS, JTable> createTableBinding(AutoBinding.UpdateStrategy strategy, SS sourceObject, Property<SS, List<E>> sourceListProperty, JTable targetJTable) {
         return new JTableBinding<E, SS, JTable>(strategy, sourceObject, sourceListProperty, targetJTable, ObjectProperty.<JTable>create(), null);
     }
-
+    
     /**
      * Creates a named {@code JTableBinding} from an object and property that resolves to a {@code List} and a direct reference to a {@code JTable}.
      *
@@ -126,7 +182,7 @@ public class SwingBindings {
     public static <E, SS> JTableBinding<E, SS, JTable> createTableBinding(AutoBinding.UpdateStrategy strategy, SS sourceObject, Property<SS, List<E>> sourceListProperty, JTable targetJTable, String name) {
         return new JTableBinding<E, SS, JTable>(strategy, sourceObject, sourceListProperty, targetJTable, ObjectProperty.<JTable>create(), name);
     }
-
+    
     
     /**
      * Creates a {@code JTableBinding} from a direct reference to a {@code List} and an object and property that resolves to a {@code JTable}.
@@ -140,7 +196,7 @@ public class SwingBindings {
     public static <E, TS> JTableBinding<E, List<E>, TS> createTableBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, TS targetObject, Property<TS, ? extends JTable> targetJTableProperty) {
         return new JTableBinding<E, List<E>, TS>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetObject, targetJTableProperty, null);
     }
-
+    
     /**
      * Creates a named {@code JTableBinding} from a direct reference to a {@code List} and an object and property that resolves to a {@code JTable}.
      *
@@ -153,8 +209,8 @@ public class SwingBindings {
     public static <E, TS> JTableBinding<E, List<E>, TS> createTableBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, TS targetObject, Property<TS, ? extends JTable> targetJTableProperty, String name) {
         return new JTableBinding<E, List<E>, TS>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetObject, targetJTableProperty, name);
     }
-
-
+    
+    
     /**
      * Creates a {@code JTableBinding} from an object and property that resolves to a {@code List} and an object and property that resolves to a {@code JTable}.
      *
@@ -168,7 +224,7 @@ public class SwingBindings {
     public static <E, SS, TS> JTableBinding<E, SS, TS> createTableBinding(AutoBinding.UpdateStrategy strategy, SS sourceObject, Property<SS, List<E>> sourceListProperty, TS targetObject, Property<TS, ? extends JTable> targetJTableProperty) {
         return new JTableBinding<E, SS, TS>(strategy, sourceObject, sourceListProperty, targetObject, targetJTableProperty, null);
     }
-
+    
     /**
      * Creates a named {@code JTableBinding} from an object and property that resolves to a {@code List} and an object and property that resolves to a {@code JTable}.
      *
@@ -182,9 +238,9 @@ public class SwingBindings {
     public static <E, SS, TS> JTableBinding<E, SS, TS> createTableBinding(AutoBinding.UpdateStrategy strategy, SS sourceObject, Property<SS, List<E>> sourceListProperty, TS targetObject, Property<TS, ? extends JTable> targetJTableProperty, String name) {
         return new JTableBinding<E, SS, TS>(strategy, sourceObject, sourceListProperty, targetObject, targetJTableProperty, name);
     }
-
-
-
+    
+    
+    
     /**
      * Creates a {@code JComboBoxBinding} from direct references to a {@code List} and {@code JComboBox}.
      *
@@ -196,7 +252,7 @@ public class SwingBindings {
     public static <E> JComboBoxBinding<E, List<E>, JComboBox> createComboBoxBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, JComboBox targetJComboBox) {
         return new JComboBoxBinding<E, List<E>, JComboBox>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetJComboBox, ObjectProperty.<JComboBox>create(), null);
     }
-
+    
     /**
      * Creates a named {@code JComboBoxBinding} from direct references to a {@code List} and {@code JComboBox}.
      *
@@ -208,32 +264,88 @@ public class SwingBindings {
     public static <E> JComboBoxBinding<E, List<E>, JComboBox> createComboBoxBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, JComboBox targetJComboBox, String name) {
         return new JComboBoxBinding<E, List<E>, JComboBox>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetJComboBox, ObjectProperty.<JComboBox>create(), name);
     }
-
-
+    
+    
+    /**
+     * Creates a {@code JComboBoxBinding} from an object and property that resolves to a {@code List} and a direct reference to a {@code JComboBox}.
+     *
+     * @param strategy the update strategy
+     * @param sourceObject the source object
+     * @param sourceListProperty a property on the source object that resolves to a {@code List}
+     * @param targetJComboBox the target {@code JComboBox}
+     * @return the {@code JComboBoxBinding}
+     */
     public static <E, SS> JComboBoxBinding<E, SS, JComboBox> createComboBoxBinding(AutoBinding.UpdateStrategy strategy, SS sourceObject, Property<SS, List<E>> sourceListProperty, JComboBox targetJComboBox) {
         return new JComboBoxBinding<E, SS, JComboBox>(strategy, sourceObject, sourceListProperty, targetJComboBox, ObjectProperty.<JComboBox>create(), null);
     }
     
+    /**
+     * Creates a named {@code JComboBoxBinding} from an object and property that resolves to a {@code List} and a direct reference to a {@code JComboBox}.
+     *
+     * @param strategy the update strategy
+     * @param sourceObject the source object
+     * @param sourceListProperty a property on the source object that resolves to a {@code List}
+     * @param targetJComboBox the target {@code JComboBox}
+     * @return the {@code JComboBoxBinding}
+     */
     public static <E, SS> JComboBoxBinding<E, SS, JComboBox> createComboBoxBinding(AutoBinding.UpdateStrategy strategy, SS sourceObject, Property<SS, List<E>> sourceListProperty, JComboBox targetJComboBox, String name) {
         return new JComboBoxBinding<E, SS, JComboBox>(strategy, sourceObject, sourceListProperty, targetJComboBox, ObjectProperty.<JComboBox>create(), name);
     }
-
-
+    
+    
+    /**
+     * Creates a {@code JComboBoxBinding} from a direct reference to a {@code List} and an object and property that resolves to a {@code JComboBox}.
+     *
+     * @param strategy the update strategy
+     * @param sourceList the source {@code List}
+     * @param targetObject the target object
+     * @param targetJComboBoxProperty a property on the target object that resolves to a {@code JComboBox}
+     * @return the {@code JComboBoxBinding}
+     */
     public static <E, TS> JComboBoxBinding<E, List<E>, TS> createComboBoxBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, TS targetObject, Property<TS, ? extends JComboBox> targetJComboBoxProperty) {
         return new JComboBoxBinding<E, List<E>, TS>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetObject, targetJComboBoxProperty, null);
     }
     
+    /**
+     * Creates a named {@code JComboBoxBinding} from a direct reference to a {@code List} and an object and property that resolves to a {@code JComboBox}.
+     *
+     * @param strategy the update strategy
+     * @param sourceList the source {@code List}
+     * @param targetObject the target object
+     * @param targetJComboBoxProperty a property on the target object that resolves to a {@code JComboBox}
+     * @return the {@code JComboBoxBinding}
+     */
     public static <E, TS> JComboBoxBinding<E, List<E>, TS> createComboBoxBinding(AutoBinding.UpdateStrategy strategy, List<E> sourceList, TS targetObject, Property<TS, ? extends JComboBox> targetJComboBoxProperty, String name) {
         return new JComboBoxBinding<E, List<E>, TS>(strategy, sourceList, ObjectProperty.<List<E>>create(), targetObject, targetJComboBoxProperty, name);
     }
-
-
+    
+    
+    /**
+     * Creates a {@code JComboBoxBinding} from an object and property that resolves to a {@code List} and an object and property that resolves to a {@code JComboBox}.
+     *
+     * @param strategy the update strategy
+     * @param sourceObject the source object
+     * @param sourceListProperty a property on the source object that resolves to a {@code List}
+     * @param targetObject the target object
+     * @param targetJComboBoxProperty a property on the target object that resolves to a {@code JComboBox}
+     * @return the {@code JComboBoxBinding}
+     */
     public static <E, SS, TS> JComboBoxBinding<E, SS, TS> createComboBoxBinding(AutoBinding.UpdateStrategy strategy, SS sourceObject, Property<SS, List<E>> sourceListProperty, TS targetObject, Property<TS, ? extends JComboBox> targetJComboBoxProperty) {
         return new JComboBoxBinding<E, SS, TS>(strategy, sourceObject, sourceListProperty, targetObject, targetJComboBoxProperty, null);
     }
-
+    
+    /**
+     * Creates a named {@code JComboBoxBinding} from an object and property that resolves to a {@code List} and an object and property that resolves to a {@code JComboBox}.
+     *
+     * @param strategy the update strategy
+     * @param sourceObject the source object
+     * @param sourceListProperty a property on the source object that resolves to a {@code List}
+     * @param targetObject the target object
+     * @param targetJComboBoxProperty a property on the target object that resolves to a {@code JComboBox}
+     * @return the {@code JComboBoxBinding}
+     */
     public static <E, SS, TS> JComboBoxBinding<E, SS, TS> createComboBoxBinding(AutoBinding.UpdateStrategy strategy, SS sourceObject, Property<SS, List<E>> sourceListProperty, TS targetObject, Property<TS, ? extends JComboBox> targetJComboBoxProperty, String name) {
         return new JComboBoxBinding<E, SS, TS>(strategy, sourceObject, sourceListProperty, targetObject, targetJComboBoxProperty, name);
     }
-
+    
 }
