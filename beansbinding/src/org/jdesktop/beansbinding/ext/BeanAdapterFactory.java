a3a6fd170852f68caf398141fa1e847fb318e157
shan_man <shan_man@729a9e19-9bed-43a5-b5a3-96140a88f07f>
2007-10-26 20:36:03 +0000

    Removed un-needed imports.

--- beansbinding/src/org/jdesktop/beansbinding/ext/BeanAdapterFactory.java
+++ beansbinding/src/org/jdesktop/beansbinding/ext/BeanAdapterFactory.java
@@ -15,7 +15,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
